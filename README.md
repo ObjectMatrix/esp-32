ef0daf0a18c695884281d22886a2358b84b51596
Ahm Asaduzzaman <Ahm.Asaduzzaman@libertymutual.com>
2020-10-13 21:42:46 -0400

    readme image resize

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # esp-32
+
+## Pin Configuration
+
+![ESP-32Pin Configuration](./esp32_pinmap.png)
---
+++ esp32/SimpleWiFiServer/SimpleWiFiServer.ino
@@ -0,0 +1,115 @@
+#include "certs.h"
+#include <WiFiClientSecure.h>
+#include <MQTTClient.h>
+#include <ArduinoJson.h>
+#include <WiFi.h>
+
+const char* ssid = "FBIHQ_EXT";
+const char* password = "nupsidupsi1204";
+
+// The name of the device. This MUST match up with the name defined in the AWS console
+#define DEVICE_NAME "esp-32-thingy"
+
+// The MQTTT endpoint for the device (unique for each AWS account but shared amongst devices within the account)
+#define AWS_IOT_ENDPOINT "a3amyzmzkjgky3-ats.iot.us-east-1.amazonaws.com"
+
+// The MQTT topic that this device should publish to
+#define AWS_IOT_TOPIC "$aws/things/" DEVICE_NAME "/shadow/update"
+
+// How many times we should attempt to connect to AWS
+#define AWS_MAX_RECONNECT_TRIES 50
+
+WiFiClientSecure net = WiFiClientSecure();
+MQTTClient client = MQTTClient(256);
+
+int hallVal = 0;
+
+void setup()
+{
+   Serial.begin(115200);
+    
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  
+    connectToAWS();
+}
+
+
+void sendJsonToAWS()
+{
+  StaticJsonDocument<128> jsonDoc;
+  JsonObject stateObj = jsonDoc.createNestedObject("state");
+  JsonObject reportedObj = stateObj.createNestedObject("reported");
+  
+  // Write the temperature & humidity. Here you can use any C++ type (and you can refer to variables)
+  hallVal = hallRead();
+  reportedObj["hall"] = hallVal;
+  Serial.println(hallVal);
+  reportedObj["wifi_strength"] = WiFi.RSSI();
+  
+  // Create a nested object "location"
+  JsonObject locationObj = reportedObj.createNestedObject("location");
+  locationObj["name"] = "Blackhole";
+
+  Serial.println("Publishing message to AWS...");
+  //serializeJson(doc, Serial);
+  char jsonBuffer[512];
+  serializeJson(jsonDoc, jsonBuffer);
+
+  client.publish(AWS_IOT_TOPIC, jsonBuffer);
+}
+
+
+void connectToAWS()
+{
+  // Configure WiFiClientSecure to use the AWS certificates we generated
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+
+  // Connect to the MQTT broker on the AWS endpoint we defined earlier
+  client.begin(AWS_IOT_ENDPOINT, 8883, net);
+
+  // Try to connect to AWS and count how many times we retried.
+  int retries = 0;
+  Serial.print("Connecting to AWS IOT");
+
+  while (!client.connect(DEVICE_NAME) && retries < AWS_MAX_RECONNECT_TRIES) {
+    Serial.print(".");
+    delay(100);
+    retries++;
+  }
+
+  // Make sure that we did indeed successfully connect to the MQTT broker
+  // If not we just end the function and wait for the next loop.
+  if(!client.connected()){
+    Serial.println(" Timeout!");
+    return;
+  }
+
+  // If we land here, we have successfully connected to AWS!
+  // And we can subscribe to topics and send messages.
+  Serial.println("Connected!");
+}
+
+
+void loop(){
+  delay(1000);
+  sendJsonToAWS();
+  delay(1000);
+}
